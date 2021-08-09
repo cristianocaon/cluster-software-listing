925225d1c0c32e7d4a8fbacbca157f918df7b043
Cristiano Caon <cristiano.caon@hotmail.com>
2021-08-09 09:21:18 -0500

    feat: add data

--- src/service/getData.js
+++ src/service/getData.js
@@ -1,25 +1,31 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-const PROTO = 'https://';
-const ADDR = 'cluster.hpcc.ttu.edu';
-const PORT = '443';
-const PAGE = '/slurm-web/summary';
+// const PROTO = 'https://';
+// const ADDR = 'cluster.hpcc.ttu.edu';
+// const PORT = '443';
+// const PAGE = '/slurm-web/summary';
 
-const getSummary = async (setData, setLoading, setError) => {
-  let url = PROTO + ADDR + ':' + PORT + PAGE;
+// const getData = async (setData, setLoading, setError) => {
+//   let url = PROTO + ADDR + ':' + PORT + PAGE;
 
-  try {
-    const { data } = await axios.get(url)
-    if (!data.error) {
-      let { charts, partitions } = data;
-      setData({ charts, partitions });
-    } else {
-      setError(data.error);
-    }
-    setLoading(false);
-  } catch (err) {
-    console.error(err.message);
-  }
-};
+//   try {
+//     const { data } = await axios.get(url)
+//     if (!data.error) {
+//       let { charts, partitions } = data;
+//       setData({ charts, partitions });
+//     } else {
+//       setError(data.error);
+//     }
+//     setLoading(false);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// };
 
-export default getSummary;
+import data from './lmod_stack.json';
+
+function getData() {
+  return data;
+}
+
+export default getData;
