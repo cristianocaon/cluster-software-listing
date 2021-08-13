7f6fe26a8717e3faa166a15ccb04a389ea5ca364
Cristiano Caon <cristiano.caon@hotmail.com>
2021-08-13 08:55:39 -0500

    feat: implement stack functionality

--- src/components/StackItem.jsx
+++ src/components/StackItem.jsx
@@ -1,20 +1,59 @@
+import React, { useState } from 'react'
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+
+import {
+  usePopupState,
+  bindHover,
+  bindPopper,
+} from 'material-ui-popup-state/hooks'
 
 const useStyles = makeStyles((theme) => ({
   button: {
     margin: '10px',
     marginLeft: '8px',
     marginRight: '8px',
-  }
+  },
+  paper: {
+    padding: '1rem',
+  },
 }));
 
-function StackItem({ data, onClick }) {
+function StackItem({ data, info, level, onClick }) {
   const classes = useStyles();
+
+  const [id, setId] = useState("");
+
+  if (!id) setId(level + "_" + data)
+
+  const [flag, setFlag] = useState(false);
+
+  const popupState = usePopupState({
+    variant: 'popper',
+    popupId: data + 'popper',
+  })
+
+  console.log(data)
 
   return (
     <>
-      <Button className={classes.button} variant="outlined" color="primary" onClick={onClick} key={data}>{data}</Button>
+      <Button
+        {...bindHover(popupState)}
+        key={data}
+        className={classes.button}
+        variant="outlined"
+        color={!flag ? "primary" : "secondary"}
+        onClick={(event) => onClick(event, parseInt(id[0]), setFlag)}>
+        {data}
+      </Button>
+      <Popper {...bindPopper(popupState)} transition>
+        <Paper className={classes.paper}>
+          <Typography>{info}</Typography>
+        </Paper>
+      </Popper>
     </>
   )
 }
--- src/components/StackList.jsx
+++ src/components/StackList.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import StackItem from './StackItem';
+import StackItem from "./StackItem";
 import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -10,42 +9,112 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     textAlign: 'center',
     marginTop: '10px',
-  }
+  },
+  button: {
+    margin: '10px',
+    marginLeft: '8px',
+    marginRight: '8px',
+  },
+  paper: {
+    padding: '1rem',
+  },
 }));
 
 function StackList({ data }) {
   const classes = useStyles();
 
-  const [values, setValues] = useState([]);
+  const [level, setLevel] = useState(0);
+  const [cards, setCards] = useState([]);
   const [selected, setSelected] = useState([]);
-  const [type, setType] = useState('versions');
+  const [prevLength, setPrevLength] = useState(0);
 
-  const handleClick = (event) => {
+  const handleClick = (event, id, setFlag) => {
     let curr = event.target.innerText.toLowerCase();
-    setSelected([...selected, curr])
+    if (level === id) {
+      let lastSelected = selected[selected.length - 1];
+      if (level % 2 === 0) {
+        if (selected.length === 0) {
+          let temp = [...selected, data[curr].versions];
+          setSelected(temp);
+          setPrevLength(temp.length)
+        } else {
+          let temp = [...selected, lastSelected[curr].versions]
+          setSelected(temp);
+          setPrevLength(temp.length)
+        }
+      } else {
+        let temp = [...selected, lastSelected[curr].child]
+        setSelected(temp);
+        setPrevLength(temp.length)
+      }
+      setLevel(level + 1);
+    } else {
+      let temp = selected.slice(0, id);
+      let lastSelected = temp.length === 0 ? [] : temp[temp.length - 1];
+      if (id % 2 === 0) {
+        if (lastSelected.length === 0) {
+          setSelected([...temp, data[curr].versions]);
+        } else {
+          setSelected([...temp, lastSelected[curr].versions]);
+        }
+      } else {
+        setSelected([...temp, lastSelected[curr].child]);
+      }
+      setLevel(id + 1);
+    }
+    setFlag(true);
   }
 
   useEffect(() => {
-    setValues([Object.keys(data)])
+    let fields = []
+    let lastSelected = selected[selected.length - 1];
+    console.log(lastSelected)
+    if (level % 2 !== 0) {
+      Object.keys(lastSelected).map(key => {
+        let path = lastSelected[key].path;
+        let module = lastSelected[key].module_name;
+        let info = "module: " + module + " path: " + path;
+        fields.push([key, info, level + 1]);
+      })
+    } else {
+      if (selected.length !== 0)
+        Object.keys(lastSelected).map(key => {
+          let info = lastSelected[key].info;
+          fields.push([key, info, level + 1]);
+        })
+    }
+    if (selected.length === prevLength) {
+      setCards([...cards, fields]);
+    } else {
+      let temp = cards.slice(0, selected.length);
+      setCards([...temp, fields]);
+    }
+  }, [selected, level, prevLength])
+
+  useEffect(() => {
+    let fields = []
+    Object.keys(data).map(key => {
+      let info = data[key].info;
+      fields.push([key, info, level]);
+    })
+    setCards([fields]);
   }, [data])
 
-  // useEffect(() => {
-  //   setValues([...values, [Object.keys(data)]])
-  // }, [selected])
+  // console.log(data)
+  // console.log(cards)
+  // console.log(selected)
 
-  console.log(selected)
-  console.log(values)
-  console.log(data)
-
-  if (values) {
+  if (cards) {
     return (
-      values.map(value => (
-        <Card className={classes.card}>
-          {value.map(el => <StackItem data={el} onClick={handleClick} />)}
+      cards.map(card => (
+        <Card key={card[0]} className={classes.card}>
+          {card.map(field => {
+            return <StackItem data={field[0]} info={field[1]} level={level} onClick={handleClick} />
+          })}
         </Card>
       ))
     )
-  }
+  } else return null;
 }
 
 export default StackList
