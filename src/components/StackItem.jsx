4cab2eb39c3de387ae4be4099358791f18c2c438
Cristiano Caon <cristiano.caon@hotmail.com>
2021-08-13 12:06:10 -0500

    style: add colors to buttons

--- src/components/StackItem.jsx
+++ src/components/StackItem.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from 'react'
-import Fade from '@material-ui/core/Fade';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-import {
-  usePopupState,
-  bindHover,
-  bindPopper,
-} from 'material-ui-popup-state/hooks'
-
 const useStyles = makeStyles((theme) => ({
-  button: {
+  button: props => ({
+    '&:hover': {
+      backgroundColor: props.backgroundColor,
+      transition: 'opacity .4s ease-out',
+      opacity: '0.5',
+    },
+    backgroundColor: props.backgroundColor,
+    color: 'white',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(200, 200, 200, .3)',
     margin: '10px',
     marginLeft: '8px',
     marginRight: '8px',
     textTransform: 'none',
-  },
+  }),
   paper: {
     padding: '1rem',
     width: '25%',
@@ -27,33 +27,33 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function StackItem({ data, info, level, onClick, getInfo }) {
-  const classes = useStyles();
+
 
   const [id, setId] = useState("");
   const [flag, setFlag] = useState(false);
 
+  const styleProps = {
+    backgroundColor: flag ?
+      '#06d6a0'
+      : '#073b4c'
+  };
+
+  const classes = useStyles(styleProps);
+
   if (!id) setId(level + "_" + data)
 
-  const popupState = usePopupState({
-    variant: 'popper',
-    popupId: data + 'popper',
-  })
-
   return (
-    <>
-      <Button
-        {...bindHover(popupState)}
-        key={data}
-        className={classes.button}
-        variant="outlined"
-        color={!flag ? "primary" : "secondary"}
-        onClick={(event) => {
-          onClick(event, parseInt(id[0]), setFlag)
-          getInfo(info);
-        }}>
-        {data}
-      </Button>
-    </>
+    <Button
+      key={data}
+      className={classes.button}
+      variant="outlined"
+      onClick={(event) => {
+        onClick(event, parseInt(id[0]), setFlag)
+        getInfo(info);
+      }}
+      disableElevation={true}>
+      {data}
+    </Button>
   )
 }
 
