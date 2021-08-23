1078ee6d1f6ffa98b2d4108c7d79b3c4a78fbace
Cristiano Caon <cristiano.e.caon@gmail.com>
2021-08-23 13:00:17 -0500

    style: change colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,19 @@ import React from 'react';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import AppBar from '@material-ui/core/AppBar';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-}));
 
 function Header({ value, partitions, handleChange }) {
-  const classes = useStyles();
   if (partitions) {
     return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Tabs value={value} onChange={handleChange} centered>
-            {
-              partitions.map(partition => {
-                return <Tab label={partition} key={partition} />
-              })
-            }
-          </Tabs>
-        </AppBar>
-      </div>
+      <AppBar position="static" style={{ background: '#0a100d', color: '#edf2f4' }}>
+        <Tabs value={value} onChange={handleChange} centered>
+          {
+            partitions.map(partition => {
+              return <Tab label={partition} key={partition} />
+            })
+          }
+        </Tabs>
+      </AppBar>
     );
   } else return null;
 }
