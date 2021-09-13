5a69d4eb0331e3ca6f484bbf149f7f5ad930ea14
Cristiano Caon <cristiano.e.caon@gmail.com>
2021-09-13 09:33:46 -0500

    fix: last button selection search

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -23,6 +23,8 @@ export default function Stack({
   handleInfoChange,
   pathSelected,
   pathCards,
+  setPathCards,
+  setPathSelected,
 }) {
   const classes = useStyles();
 
@@ -126,7 +128,7 @@ export default function Stack({
     if (currRow) {
       for (let i = 0; i < currRow.length; i++) {
         let currBtn = currRow[i];
-        if (currBtn[2] === true) {
+        if (currBtn[2] === true && typeof pathCards === 'undefined') {
           currBtn[2] = false;
         }
         if (currBtn[0] === clicked) {
@@ -134,6 +136,8 @@ export default function Stack({
         }
       }
       setChanged((prevState) => prevState + 1);
+      setPathCards();
+      setPathSelected();
     }
   }, [cards, clicked, level]);
 
