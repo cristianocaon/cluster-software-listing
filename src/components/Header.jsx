bdfce3c2ca3ec4eeabfc4ac41398720348242d6e
Cristiano Caon <cristiano.e.caon@gmail.com>
2021-09-15 12:50:00 -0500

    feat: add help button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,45 @@ import React from 'react';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import HelpIcon from '@material-ui/icons/Help';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function Header({ value, partitions, onChange }) {
+const useStyles = makeStyles(() => ({
+  root: {
+    position: 'static',
+    background: '#0a100d',
+    color: '#edf2f4',
+    alignItems: 'center',
+  },
+}));
+
+export default function Header({
+  value,
+  partitions,
+  onChange,
+  handleInfoDisplay,
+}) {
+  const classes = useStyles();
+
   if (partitions) {
     return (
-      <AppBar
-        position="static"
-        style={{ background: '#0a100d', color: '#edf2f4' }}
-      >
-        <Tabs value={value} onChange={onChange} centered>
-          {partitions.map((partition) => {
-            return <Tab label={partition} key={partition} />;
-          })}
-        </Tabs>
+      <AppBar className={classes.root}>
+        <Toolbar>
+          <Tabs value={value} onChange={onChange} centered>
+            {partitions.map((partition) => {
+              return <Tab label={partition} key={partition} />;
+            })}
+            <IconButton
+              color="inherit"
+              component="span"
+              onClick={handleInfoDisplay}
+            >
+              <HelpIcon />
+            </IconButton>
+          </Tabs>
+        </Toolbar>
       </AppBar>
     );
   } else return null;
